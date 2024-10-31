122247eb5e1a527a7f0c2c25d40b42125e3b493a
Manuel Werder <manuelwerder@hotmail.com>
2024-10-31 13:32:29 +0100

    Added Header component styles and image

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {}
