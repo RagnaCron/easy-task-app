67fb1b90676e90d1a6fb76235498a7a98bc2b7e8
Manuel Werder <manuelwerder@hotmail.com>
2024-11-04 12:53:54 +0100

    Refactoring to use Inputs on UserComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 import {HeaderComponent} from './header/header.component';
 import {UserComponent} from './user/user.component';
+import {DUMMY_USERS} from './dummy-user';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,5 @@ import {UserComponent} from './user/user.component';
 })
 export class AppComponent {
   title = 'easy-task-app';
+  users = DUMMY_USERS;
 }
